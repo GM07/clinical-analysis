bd41daaf535df1cc99b7a925091fe9a27e4b53ad
GM07 <mehgaya@gmail.com>
2025-03-06 16:21:34 -0500

    Added support for multiple verbalization datasets

--- scripts/domain_adaptation/verbalize.py
+++ scripts/domain_adaptation/verbalize.py
@@ -17,27 +17,29 @@ logging.basicConfig(
 parser = ArgumentParser(description='Program that prunes the extractions of a dataset. \
                         The dataset will be saved in the same file as the input file, but with a new column containing the pruned extractions.')
 
-parser.add_argument('--dataset', type=str, required=True, help='Path to extraction dataset file (csv file)')
+parser.add_argument('--dataset', type=str, nargs='+', required=True, help='Path to extraction dataset file (csv file)')
 parser.add_argument('--model_path', type=str, required=True, help='Path to huggingface model file')
 parser.add_argument('--tokenizer_path', type=str, required=True, help='Path to huggingface tokenizer file')
 parser.add_argument('--snomed', type=str, required=True, help='Path to SNOMED file (owl file)')
 parser.add_argument('--snomed_cache', type=str, required=True, help='Path to SNOMED cache file')
-parser.add_argument('--output_dataset', type=str, required=True, help='Path to output dataset file')
+parser.add_argument('--output_dataset', type=str, nargs='+', required=True, help='Path to output dataset file')
 
 def main():
 
     args = parser.parse_args()
 
     print('Called with arguments : ', args)
+    assert len(args.dataset) == len(args.output_dataset), 'The number of dataset and output dataset must be the same'
 
     snomed = Snomed(args.snomed, args.snomed_cache)
 
     columns = ['constrained_ecg', 'constrained_nursing', 'constrained_radiology', 'constrained_physician_']
     verbalizer = Verbalizer(model_path=args.model_path, tokenizer_path=args.tokenizer_path, input_columns=columns, snomed=snomed)
 
-    pruned_dataset = PrunedConceptDataset(columns=columns, dataset_path=args.dataset)
-    verbalized_dataset = verbalizer.verbalize_dataset(pruned_dataset)
-    verbalized_dataset.to_csv(args.output_dataset, index=False)
+    for dataset_path, output_path in zip(args.dataset, args.output_dataset):
+        pruned_dataset = PrunedConceptDataset(columns=columns, dataset_path=dataset_path)
+        verbalized_dataset = verbalizer.verbalize_dataset(pruned_dataset)
+        verbalized_dataset.to_csv(output_path, index=False)
 
 if __name__ == '__main__':
     main()
--- src/domain_adaptation/verbalizer.py
+++ src/domain_adaptation/verbalizer.py
@@ -1,7 +1,7 @@
 
 from datasets import Dataset as HuggingFaceDataset
 
-from src.data.dataset import PrunedConceptDataset, VerbalizedExtractionDataset
+from src.data.dataset import PrunedConceptDataset
 from src.domain_adaptation.prompt_generator import PrunedConceptPromptGenerator
 from src.ontology.snomed import Snomed
 from src.pipelines.dataset_inference_pipeline import ModelDatasetInferencePipeline
@@ -14,14 +14,14 @@ class Verbalizer:
         self.snomed = snomed
         self.model_path = model_path
         self.tokenizer_path = tokenizer_path
+        self.pipeline = ModelDatasetInferencePipeline(model_path=self.model_path, tokenizer_path=self.tokenizer_path)
 
     def verbalize_dataset(self, dataset: PrunedConceptDataset):
         self.prompt_generator = PrunedConceptPromptGenerator(mimic=dataset.data, snomed=self.snomed, input_columns=self.input_columns)
         data = self.prompt_generator.generate_prompts()
 
-        # More efficient for inference
+        # More efficient for inference to convert to a HuggingFaceDataset
         hf_dataset = HuggingFaceDataset.from_pandas(data)
-        self.pipeline = ModelDatasetInferencePipeline(model_path=self.model_path, tokenizer_path=self.tokenizer_path)
         for input_column in self.input_columns:
             hf_dataset = self.pipeline(hf_dataset, max_new_tokens=512, input_column=f'{input_column}_verbalizer_prompt', output_column=f'{input_column}_verbalized')
         return hf_dataset
