b2085525b05bb49ceffd8c525a0993ec068e3162
GM07 <mehgaya@gmail.com>
2025-03-10 17:35:51 -0400

    Added rows to chat option

--- scripts/inference_dataset.py
+++ scripts/inference_dataset.py
@@ -5,6 +5,7 @@ from datasets import Dataset as HuggingFaceDataset
 
 from src.data.dataset import DatasetPartition
 from src.pipelines.dataset_inference_pipeline import ModelDatasetInferencePipeline
+from src.data.utils import rows_to_chat
 
 logging.basicConfig(
     level=logging.INFO,
@@ -19,6 +20,7 @@ parser.add_argument('--tokenizer', type=str, required=False, default=None, help=
 parser.add_argument('--dataset', type=str, required=True, help='Path to dataset (csv)')
 parser.add_argument('--output_path', type=str, required=True, help='Path where the output dataset will be saved')
 parser.add_argument('--max_rows_to_process', type=int, default=100, help='Maximum number of rows to process')
+parser.add_argument('--rows_to_chat', type=bool, default=True, help='Rows to chat')
 parser.add_argument('--input_column', type=str, default='PROMPT', help='Column to use as input')
 parser.add_argument('--output_column', type=str, default='OUTPUT', help='Column to use as output')
 
@@ -39,7 +41,8 @@ def main():
         dataset, 
         input_column=args.input_column, 
         output_column=args.output_column,
-        max_rows_to_process=args.max_rows_to_process
+        max_rows_to_process=args.max_rows_to_process,
+        rows_to_chat=rows_to_chat if args.rows_to_chat else None
     )
 
     output_dataset.to_csv(args.output_path, index=False)
